ab60985b3a801a42237e3915b0d57223c8efbe1c
shehanjay2000 <shehanjay2000@gmail.com>
2025-04-23 19:56:54 +0530

    database conneted

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,14 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(3000, () => console.log('Server running on http://localhost:3000'));
-  })
-  .catch(err => console.log(err));
+const mongoUrl = "mongodb+srv://shehanjay2000:123@cluster0.p8xekiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(mongoUrl,{})
+const connection = mongoose.connection;
+
+connection.once("open",()=>{
+    console.log("Database connected successfully")
+})
+
 
 module.exports = app; // for testing
