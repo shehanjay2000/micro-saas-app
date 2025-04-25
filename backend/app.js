63df81df6865d2ebeffe59b8834cde70d86dc7b1
shehanjay2000 <shehanjay2000@gmail.com>
2025-04-25 23:30:23 +0530

    feat: implement logging with Winston and add request logging middleware

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,15 @@ import cors from 'cors';
 import todoRoutes from './routes/todoRoutes.js';
 
 const app = express();
+const logger = import('./logger.js')
 app.use(cors());
 app.use(json());
 app.use('/api/todos', todoRoutes);
+
+app.use((req, res, next) => {
+    logger.info(`${req.method} ${req.url}`);
+    next();
+  });
 
 const mongoUrl = "mongodb+srv://shehanjay2000:123@cluster0.p8xekiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
